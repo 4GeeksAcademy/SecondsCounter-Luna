0aa7eba0d86b55383cad678dde1d4b022a7d77c6
LunaB28 <lunab2815@gmail.com>
2025-04-23 20:15:26 +0000

    Creacion del contador Hice desde mis conocimientos iniciales lo necesario para hacer que se parezca al ejemplo

---
+++ src/js/components/Digit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Digit = (props) => {
+  return (
+    <div className="d-inline-block text-center">
+        
+      <h1 className='text-white bg-dark p-2 m-2'> {props.num}</h1> 
+      </div>
+  )
+}
+
+export default Digit
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,25 +2,30 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Digit from "./Digit";
 
 //create your first component
-const Home = () => {
+const Home = ({ segundos }) => {
+	let unidades = segundos % 10;
+	let segundosI = segundos / 10;
+	let decenas = Math.floor(segundosI % 10);
+	let segundosII = segundosI / 10;
+	let centenas = Math.floor(segundosII % 10)
+	let segundosIII = segundosII / 10;
+	let unidadesDeMil = Math.floor(segundosIII % 10);
+	let segundosIV = segundosIII / 10;
+	let decenasDeMil = Math.floor(segundosIV % 10)
+	let segundosV = segundosIV / 10;
+	let centenasDeMil = Math.floor(segundosV % 10)
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center bg-black p-">
+			<Digit num=<i className="fa-solid fa-clock" style={{color: "#ffffff;"}}></i>/>
+			<Digit num={centenasDeMil} />
+			<Digit num={decenasDeMil} />
+			<Digit num={unidadesDeMil} />
+			<Digit num={centenas} />
+			<Digit num={decenas} />
+			<Digit num={unidades} />
 		</div>
 	);
 };
